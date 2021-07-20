62b1a03a0e7cbf5164768aefe8aaadbf13f39368
남상혁 <sanam@namsanghyeog-ui-MacBookPro.local>
2021-07-20 17:44:31 +0900

    describe 블록

--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -8,23 +8,25 @@ const MockTodoFooter = ({ numberOfIncompleteTasks }) => (
   </BrowserRouter>
 );
 
-it('should render the correct amount of incomplete tasks', async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={5} />);
-  const paragraphElement = screen.getByText(/5 tasks left/i);
-  expect(paragraphElement).toBeInTheDocument();
-});
+describe('TodoFooter Test', () => {
+  it('should render the correct amount of incomplete tasks', async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+    const paragraphElement = screen.getByText(/5 tasks left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
 
-it('should render the correct singluar syntax when the amount is 1', async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeInTheDocument();
-  expect(paragraphElement).toBeTruthy();
-  expect(paragraphElement).toBeVisible();
-});
+  it('should render the correct singluar syntax when the amount is 1', async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeInTheDocument();
+    expect(paragraphElement).toBeTruthy();
+    expect(paragraphElement).toBeVisible();
+  });
 
-it('should render the correct plural syntax when the amount is more than 1', async () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={2} />);
-  const paragraphElement = screen.getByText(/2 tasks left/i);
-  expect(paragraphElement).toBeInTheDocument();
-  expect(paragraphElement.textContent).toBe('2 tasks left');
+  it('should render the correct plural syntax when the amount is more than 1', async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={2} />);
+    const paragraphElement = screen.getByText(/2 tasks left/i);
+    expect(paragraphElement).toBeInTheDocument();
+    expect(paragraphElement.textContent).toBe('2 tasks left');
+  });
 });
